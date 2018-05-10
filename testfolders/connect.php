b280e7acd19c13b4c557a93ba155e4b88f3f5478
cxmellellow <38735871+cxmellellow@users.noreply.github.com>
2018-05-10 12:06:39 -0700

    Create connect.php

---
+++ testfolders/connect.php
@@ -0,0 +1,31 @@
+    <?php
+        $servername = "localhost"; //local host works sinces its on the current server.
+        $username = "";//enter in username that has access to database
+        $password = "";//enter usernames password
+        $dbname = ""; //enter name of databse on server
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+
+        else {
+            echo "Connection Successful";
+        }
+
+
+        "INSERT INTO contact_table (Client, Email, Reason, Comment)
+        VALUES ('".$POST["name"]."','". $POST['email']."','".$POST["reason"]."','".$POST["message"]."')";
+
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+
+?>
