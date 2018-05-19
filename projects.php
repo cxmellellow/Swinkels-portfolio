10a7ac6f88f347a66d6bcfe8a211113cca863518
Christine <christine.beach@pcc.edu>
2018-05-19 16:27:12 -0700

    added css to create horizontal

--- projects.php
+++ projects.php
@@ -2,8 +2,47 @@
     <h3>Header 3</h3>
 </header>
 <article>
-    <section class="filler">
-        <img src="img/dummy-img.png">
-        <figcaption>This will hold slider area for projects</figcaption>
-    </section>
+    <section class="slider">
+        <section class="row scroll">
+          <section class="column filler">
+            <div class="card">
+              <img src="img/dummy-img.png" alt="Jane" style="width:100%">
+              <div class="container">
+                <h2>Jane Doe</h2>
+                <p class="title">CEO &amp; Founder</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>example@example.com</p>
+                <p><button class="button">Contact</button></p>
+              </div>
+            </div>
+          </section><!--/.column-->
+
+          <section class="column filler">
+            <div class="card">
+              <img src="img/dummy-img.png" alt="Mike" style="width:100%">
+              <div class="container">
+                <h2>Mike Ross</h2>
+                <p class="title">Art Director</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>example@example.com</p>
+                <p><button class="button">Contact</button></p>
+              </div>
+            </div>
+          </section><!--/.column-->
+
+          <section class="column filler">
+            <div class="card">
+              <img src="img/dummy-img.png" alt="John" style="width:100%">
+              <div class="container">
+                <h2>John Doe</h2>
+                <p class="title">Designer</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>example@example.com</p>
+                <p><button class="button">Contact</button></p>
+              </div>
+            </div>
+          </section><!--/.column-->
+            
+        </section><!--/.row-->
+    </section> <!--/.filler-->
 </article>
